becd95c85b884c7d1be82dfa097f689da9a6136b
Xuedong Shang <shang.xuedong@yahoo.fr>
2018-03-14 17:28:36 +0100

    Removed some spaces.

--- clustering/algos.py
+++ clustering/algos.py
@@ -23,7 +23,7 @@ def pair_distance(cluster_list, idx1, idx2):
     Helper function that computes Euclidean distance between two clusters in a list
 
     Input: cluster_list is list of clusters, idx1 and idx2 are integer indices for two clusters
-    
+
     Output: tuple (dist, idx1, idx2) where dist is distance between
     cluster_list[idx1] and cluster_list[idx2]
     """
@@ -35,9 +35,9 @@ def slow_closest_pair(cluster_list):
     Compute the distance between the closest pair of clusters in a list (slow)
 
     Input: cluster_list is the list of clusters
-    
+
     Output: tuple of the form (dist, idx1, idx2) where the centers of the clusters
-    cluster_list[idx1] and cluster_list[idx2] have minimum distance dist.       
+    cluster_list[idx1] and cluster_list[idx2] have minimum distance dist.
     """
     current_dist, current_idx1, current_idx2 = float('inf'), -1, -1
 
@@ -59,13 +59,13 @@ def fast_closest_pair(cluster_list):
 
     Input: cluster_list is list of clusters SORTED such that horizontal positions of their
     centers are in ascending order
-    
+
     Output: tuple of the form (dist, idx1, idx2) where the centers of the clusters
-    cluster_list[idx1] and cluster_list[idx2] have minimum distance dist.       
+    cluster_list[idx1] and cluster_list[idx2] have minimum distance dist.
     """
     number_clusters = len(cluster_list)
     cluster_list.sort(key = lambda cluster: cluster.horiz_center())
-    
+
     if number_clusters < 4:
         return slow_closest_pair(cluster_list)
     else:
@@ -82,21 +82,21 @@ def fast_closest_pair(cluster_list):
         closest_strip = closest_pair_strip(cluster_list, mid, dist)
         if closest_strip[0] < dist:
             dist, idx1, idx2 = closest_strip
-            
+
     return (dist, idx1, idx2)
 
 
 def closest_pair_strip(cluster_list, horiz_center, half_width):
     """
     Helper function to compute the closest pair of clusters in a vertical strip
-    
+
     Input: cluster_list is a list of clusters produced by fast_closest_pair
     horiz_center is the horizontal position of the strip's vertical center line
     half_width is the half the width of the strip (i.e; the maximum horizontal distance
     that a cluster can lie from the center line)
 
     Output: tuple of the form (dist, idx1, idx2) where the centers of the clusters
-    cluster_list[idx1] and cluster_list[idx2] lie in the strip and have minimum distance dist.       
+    cluster_list[idx1] and cluster_list[idx2] lie in the strip and have minimum distance dist.
     """
     strip = list()
     for cluster in cluster_list:
@@ -105,7 +105,7 @@ def closest_pair_strip(cluster_list, horiz_center, half_width):
     strip.sort(key = lambda cluster: cluster[0].vert_center())
     length = len(strip)
     dist, idx1, idx2 = float('inf'), -1, -1
-    
+
     for idx_u in range(length-1):
         for idx_v in range(idx_u+1, min(idx_u+4, length)):
             uv_dist = strip[idx_u][0].distance(strip[idx_v][0])
@@ -117,10 +117,10 @@ def closest_pair_strip(cluster_list, horiz_center, half_width):
                 else:
                     idx1 = strip[idx_v][1]
                     idx2 = strip[idx_u][1]
-                    
+
     return (dist, idx1, idx2)
-            
-    
+
+
 ######################################################################
 # Code for hierarchical clustering
 
@@ -129,7 +129,7 @@ def hierarchical_clustering(cluster_list, num_clusters):
     """
     Compute a hierarchical clustering of a set of clusters
     Note: the function may mutate cluster_list
-    
+
     Input: List of clusters, integer number of clusters
     Output: List of clusters whose length is num_clusters
     """
@@ -141,23 +141,23 @@ def hierarchical_clustering(cluster_list, num_clusters):
         cluster_list.pop(closest_pair[2])
         # resorting
         cluster_list.sort(key = lambda cluster: cluster.horiz_center())
-        
+
     return cluster_list
 
 
 ######################################################################
 # Code for k-means clustering
 
-    
+
 def kmeans_clustering(cluster_list, num_clusters, num_iterations):
     """
     Compute the k-means clustering of a set of clusters
     Note: the function may not mutate cluster_list
-    
+
     Input: List of clusters, integers number of clusters and number of iterations
     Output: List of clusters whose length is num_clusters
     """
-    # position initial clusters at the location of clusters with largest populations    
+    # position initial clusters at the location of clusters with largest populations
     sorted_cluster = list(cluster_list)
     sorted_cluster.sort(key = lambda cluster: cluster.total_population())
     centers = list()
@@ -166,7 +166,7 @@ def kmeans_clustering(cluster_list, num_clusters, num_iterations):
         vert = sorted_cluster[len(sorted_cluster)-idx_k].vert_center()
         centers.append((horiz, vert))
     assert len(centers) == num_clusters # force k centers
-    
+
     # clustering
     for idx_i in range(num_iterations):
         # initialize k empty clusters
@@ -191,7 +191,6 @@ def kmeans_clustering(cluster_list, num_clusters, num_iterations):
                 horiz = clusters[idx_k].horiz_center()
                 vert = clusters[idx_k].vert_center()
                 centers[idx_k] = (horiz, vert)
-    
-    return clusters
 
+    return clusters
 
